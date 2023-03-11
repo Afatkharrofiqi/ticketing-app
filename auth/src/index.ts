fa5abb40af6b693366f268c5c00c506555df2c74
fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2023-03-11 23:05:53 +0700

    feat: handle errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,21 +1,26 @@
+import { errorHandler } from './middlewares/error-handler';
 import express from 'express';
 import { json } from 'body-parser'
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
-
 
 app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(currentUserRouter);
-app.get('/', (req, res) => {
-  res.send('auth services');
+
+// all means all method by GET, POST, PUT, PATCH and DELETE will be handled
+app.all('*', () => {
+  throw new NotFoundError()
 });
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
